7be0f3806377feeb6d03433ca2c7765178070349
Nicolas Granelli <nicolasgranelli@sitio54.com.ar>
2011-10-23 22:15:06 -0300

    Set objects as cookie value

--- test/web.js
+++ test/web.js
@@ -15,3 +15,45 @@ test('remove', function() {
   monster.remove(name);
   equal(monster.get(name), null);
 });
+test('set object', function(){
+  var name = 'name';
+  var value = {some:'value'};
+  var days = 1;
+  monster.set(name, value, days);
+  deepEqual(monster.get(name), value);
+});
+test('set array', function(){
+  var name = 'name';
+  var value = ['some','value'];
+  var days = 1;
+  monster.set(name, value, days);
+  deepEqual(monster.get(name), value);
+});
+test('set undefined', function(){
+  var name = 'name';
+  var value = undefined;
+  var days = 1;
+  monster.set(name, value, days);
+  deepEqual(monster.get(name), value);
+});
+test('set null', function(){
+  var name = 'name';
+  var value = null;
+  var days = 1;
+  monster.set(name, value, days);
+  deepEqual(monster.get(name), value);
+});
+test('set string edge case string starting with "["', function(){
+  var name = 'name';
+  var value = "[something edgy";
+  var days = 1;
+  monster.set(name, value, days);
+  deepEqual(monster.get(name), value);
+});
+test('set string edge case string starting with "{"', function(){
+  var name = 'name';
+  var value = '{something edgy';
+  var days = 1;
+  monster.set(name, value, days);
+  deepEqual(monster.get(name), value);
+});
