33a9186c9ecf3a090ec9fa40671746599447f14f
Greg Allen <me@jga.me>
2012-12-06 14:32:14 -0800

    built

---
+++ HISTORY.md
@@ -0,0 +1,10 @@
+
+0.0.3 / 2012-12-06 
+==================
+
+  * watch test files
+  * change to tdd style
+  * removed unneccessary files
+  * added grunt support
+  * swapped out chai with assert
+
--- dist/monster.js
+++ dist/monster.js
@@ -1,10 +1,10 @@
 /*!
-  * Cookie Monster - A javascript cookie library 
-  * v0.0.3
-  * https://github.com/jgallen23/cookie-monster
-  * copyright JGA 2011
-  * MIT License
-  */
+ * cookie-monster - a simple cookie library
+ * v0.0.3
+ * https://github.com/jgallen23/cookie-monster
+ * copyright JGA 2012
+ * MIT License
+*/
 
 var monster = {
   set: function(name, value, days, path) {
