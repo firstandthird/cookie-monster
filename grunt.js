644ea56f706eb0a072bd223473a8ff5e2e46cfa2
Greg Allen <me@jga.me>
2012-12-06 14:23:48 -0800

    added grunt support

---
+++ grunt.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    info: '<json:package.json>',
+    meta: {
+      banner: '/*!\n'+
+              ' * <%= info.name %> - <%= info.description %>\n'+
+              ' * v<%= info.version %>\n'+
+              ' * <%= info.homepage %>\n'+
+              ' * copyright <%= info.copyright %> <%= grunt.template.today("yyyy") %>\n'+
+              ' * <%= info.license %> License\n'+
+              '*/'
+    },
+    lint: {
+      all: 'lib/monster.js'
+    },
+    concat: {
+      dist: {
+        src: ['<banner>', 'lib/monster.js'],
+        dest: 'dist/monster.js'
+      },
+    },
+    min: {
+      dist: {
+        src: ['<banner>', 'dist/monster.js'],
+        dest: 'dist/monster.min.js'
+      }
+    },
+    simplemocha: {
+      all: {
+        src: 'test/**/*.test.js',
+        options: {
+          ui: 'bdd',
+          reporter: 'list',
+          growl: true
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: 'lib/monster.js',
+        tasks: 'concat min' 
+      }
+    },
+    server:{
+      port: 8000,
+      base: '.'
+    }
+  });
+  grunt.registerTask('default', 'lint concat min');
+  grunt.registerTask('dev', 'server watch');
+}
